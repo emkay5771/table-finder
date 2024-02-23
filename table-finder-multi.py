7f451a3743ffb2fb87e5402fa6b2c2000f958296
Mendy Kay <52011243+emkay5771@users.noreply.github.com>
2024-02-23 18:39:56 +0000

    background edit

--- table-finder-multi.py
+++ table-finder-multi.py
@@ -20,7 +20,7 @@ background_image=(f"""
         background-image: url("https://github.com/emkay5771/table-finder/blob/main/images/background-darker-er.jpg?raw=true");
         background-size: 100vw;  # This sets the size to cover 100% of the viewport width and height
         background-position: 50%, 50%;  
-        background-repeat: space repeat;
+        
         backdrop-filter: blur(5px); 
     }}
     </style>
