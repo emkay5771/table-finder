44bf4424b5921365c6a80d774597c7a65a5efc3e
Mendy Kay <52011243+emkay5771@users.noreply.github.com>
2024-02-23 18:46:09 +0000

    fixes

--- table-finder-multi.py
+++ table-finder-multi.py
@@ -18,9 +18,9 @@ background_image=(f"""
     <style>
     [data-testid="stAppViewContainer"] {{
         background-image: url("https://github.com/emkay5771/table-finder/blob/main/images/background-darker-er.jpg?raw=true");
-        background-size: 100vh, 100vw;  # This sets the size to cover 100% of the viewport width and height
-        background-position: center;  
-        background-repeat: no-repeat;
+        background-size: 100vw;  # This sets the size to cover 100% of the viewport width and height
+        background-position: 50%, 50%;  
+        background-repeat: space repeat;
         backdrop-filter: blur(5px); 
     }}
     </style>
