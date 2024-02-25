3c2f5bd917dabefb94adcdcf28bad50eea2cd9d3
Mendy Kay <52011243+emkay5771@users.noreply.github.com>
2024-02-25 04:28:41 +0000

    more markdown

--- table-finder-multi.py
+++ table-finder-multi.py
@@ -91,3 +91,16 @@ footer {
     }   
     </style>
     """, unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
